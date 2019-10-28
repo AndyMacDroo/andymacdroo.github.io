6d1566a36cbbec1cc08dc0af9ab70f5922f34bfe
Andy Macdonald <hello@andymacdonald.dev>
2019-10-28 22:47:52 +0000

    Prod build

---
+++ precache-manifest.ac465ee667b2dfbd1748df6f6a95634a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f4ef7144b1bef8a0c0444ab8c5c3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98f993be343d5445192f",
+    "url": "/static/css/main.d1aaebcc.chunk.css"
+  },
+  {
+    "revision": "4daeb19423b7513f11a6",
+    "url": "/static/js/2.310258f8.chunk.js"
+  },
+  {
+    "revision": "98f993be343d5445192f",
+    "url": "/static/js/main.afaa3f7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
