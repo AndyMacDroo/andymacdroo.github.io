4dfc2452720e85d519092e4e8b3bf08d0bccc0c3
Andy Macdonald <hello@andymacdonald.dev>
2019-10-29 11:42:56 +0000

    Add build folder

--- precache-manifest.ae87c4c035b7531fcba63a2a34cbcc83.js
+++ precache-manifest.ae87c4c035b7531fcba63a2a34cbcc83.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf3cbd276b2a89ddd845e4914c940dc6",
+    "revision": "f1d471213d7025b7179da08183bbfc2b",
     "url": "/index.html"
   },
   {
-    "revision": "695b223c52fb4649b6d3",
-    "url": "/static/css/main.b1ce36a4.chunk.css"
+    "revision": "929fee2f20f0b40eacaa",
+    "url": "/static/css/main.0653b38a.chunk.css"
   },
   {
     "revision": "4daeb19423b7513f11a6",
     "url": "/static/js/2.310258f8.chunk.js"
   },
   {
-    "revision": "695b223c52fb4649b6d3",
-    "url": "/static/js/main.2e8c14a8.chunk.js"
+    "revision": "929fee2f20f0b40eacaa",
+    "url": "/static/js/main.3b928e3f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
