297b2aba28f5f2ecb35daf3c90c652fde9e0fd8e
Andy Macdonald <hello@andymacdonald.dev>
2019-10-31 16:08:17 +0000

    Update build folder

---
+++ build/precache-manifest.4238f94ed6190c26000a66a0157f0714.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae765102099ea732a3c800657fd7621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6ecc433784a5164f85e",
+    "url": "/static/css/main.fcdd32ef.chunk.css"
+  },
+  {
+    "revision": "ce47772741c208e00be7",
+    "url": "/static/js/2.e17f6542.chunk.js"
+  },
+  {
+    "revision": "d6ecc433784a5164f85e",
+    "url": "/static/js/main.8314816a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
